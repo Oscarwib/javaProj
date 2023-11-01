57446666ce332733cf02558d932a2dca907a94a3
Oscar Wiberg <oscarwiberg@Oscars-MacBook-Air-3.local>
2023-11-01 14:27:33 +0100

    nya

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-
+//FFffffffff
 /**
  * This Class is the entry point of the application.
  * <p>
